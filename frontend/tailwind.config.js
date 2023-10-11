9196f1653cd4fd7d57266f7b7920c1751ac693ee
Kacper Stępień <kacper2007x48@gmail.com>
2023-10-11 16:17:01 +0200

    frontend: install needed packages

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "1800px",
+      },
+    },
+  },
+  plugins: [],
+};
