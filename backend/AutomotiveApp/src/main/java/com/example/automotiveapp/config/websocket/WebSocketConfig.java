8e89dd78c4e8b93dee6d15b400123f8d8c51893b
grzegorzsz24 <grzesio.szymanek@gmail.com>
2023-10-27 18:51:52 +0200

    backend: update socket config

--- backend/AutomotiveApp/src/main/java/com/example/automotiveapp/config/websocket/WebSocketConfig.java
+++ backend/AutomotiveApp/src/main/java/com/example/automotiveapp/config/websocket/WebSocketConfig.java
@@ -17,8 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws");
-
+        registry.addEndpoint("/ws").setAllowedOrigins("http://127.0.0.1:5173").withSockJS();
     }
 }
 
