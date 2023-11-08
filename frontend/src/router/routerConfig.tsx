b187bb3a12d35982e7a74de2f9558a40b5e33757
Kacper Stępień <kacper2007x48@gmail.com>
2023-11-08 23:50:26 +0100

    frontend: refactor: move router configuration to separate file for better modularity

---
+++ frontend/src/router/routerConfig.tsx
@@ -0,0 +1,168 @@
+import AddArticlePage from "../pages/Articles/AddArticlePage";
+import AddForumPage from "../pages/Forums/AddForumPage";
+import AppLayout from "../pages/AppLayout";
+import ArticlePage from "../pages/Articles/ArticlePage";
+import ArticlePageLayout from "../pages/Articles/ArticlePageLayout";
+import ArticlesPage from "../pages/Articles/ArticlesPage";
+import ForumPage from "../pages/Forums/ForumPage";
+import ForumPageLayout from "../pages/Forums/ForumPageLayout";
+import ForumsPage from "../pages/Forums/ForumsPage";
+import LoginPage from "../pages/LoginPage";
+import MainPage from "../pages/MainPage";
+import MainPageLayout from "../pages/MainPageLayout";
+import MyArticlesPage from "../pages/Articles/MyArticlesPage";
+import MyForumsPage from "../pages/Forums/MyForumsPage";
+import OutletLayout from "../pages/OutletLayout";
+import ProtectedRoute from "./ProtectedRoute";
+import ReceivedInvitationsPage from "../pages/Friends/ReceivedInvitationsPage";
+import RegisterPage from "../pages/RegisterPage";
+import SentInvitationsPage from "../pages/Friends/SentInvitationsPage";
+import UserCloseAccountPage from "../pages/UserSettings/UserCloseAccountPage";
+import UserFriendsPage from "../pages/Friends/UserFriendsPage";
+import UserFriendsPageLayout from "../pages/Friends/UserFriendsPageLayout";
+import UserFriendsSuggestionsPage from "../pages/Friends/UserFriendsSuggestionsPage";
+import UserPage from "../pages/UserPage";
+import UserSettingsDataPage from "../pages/UserSettings/UserSettingsDataPage";
+import UserSettingsPageLayout from "../pages/UserSettings/UserSettingsPageLayout";
+import UserSettingsPhoto from "../pages/UserSettings/UserSettingsPhoto";
+import UserSettingsPrivacyPage from "../pages/UserSettings/UserSettingsPrivacyPage";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <ProtectedRoute element={<MainPageLayout />} />,
+        children: [
+          {
+            path: "/",
+            element: <MainPage />,
+          },
+          {
+            path: "/articles",
+            element: <ArticlePageLayout />,
+            children: [
+              {
+                path: "",
+                element: <ArticlesPage />,
+              },
+              {
+                path: "add",
+                element: <AddArticlePage />,
+              },
+              {
+                path: "my",
+                element: <MyArticlesPage />,
+              },
+              {
+                path: ":article",
+                element: <ArticlePage />,
+              },
+            ],
+          },
+          {
+            path: "/events",
+            element: <p>Wydarzenia</p>,
+          },
+          {
+            path: "/friends",
+            element: <UserFriendsPageLayout />,
+            children: [
+              {
+                path: "",
+                element: <UserFriendsPage />,
+              },
+              {
+                path: "invitations",
+                element: <ReceivedInvitationsPage />,
+              },
+              {
+                path: "suggestions",
+                element: <UserFriendsSuggestionsPage />,
+              },
+              {
+                path: "pending",
+                element: <SentInvitationsPage />,
+              },
+            ],
+          },
+          {
+            path: "/bookmarks",
+            element: <p>Zapisane</p>,
+          },
+          {
+            path: "/forums",
+            element: <ForumPageLayout />,
+            children: [
+              {
+                path: "",
+                element: <ForumsPage />,
+              },
+              {
+                path: ":forum",
+                element: <ForumPage />,
+              },
+              {
+                path: "add",
+                element: <AddForumPage />,
+              },
+              {
+                path: "my",
+                element: <MyForumsPage />,
+              },
+            ],
+          },
+          {
+            path: "/me",
+            element: <OutletLayout />,
+            children: [
+              {
+                path: "",
+                element: <UserPage />,
+              },
+              {
+                path: "settings",
+                element: <UserSettingsPageLayout />,
+                children: [
+                  {
+                    path: "data",
+                    element: <UserSettingsDataPage />,
+                  },
+                  {
+                    path: "privacy",
+                    element: <UserSettingsPrivacyPage />,
+                  },
+                  {
+                    path: "close",
+                    element: <UserCloseAccountPage />,
+                  },
+                  {
+                    path: "photo",
+                    element: <UserSettingsPhoto />,
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            path: "/user/:nickname",
+            element: <UserPage />,
+          },
+        ],
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+    ],
+  },
+]);
+
+export default router;
