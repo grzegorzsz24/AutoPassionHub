dd62d1ead6bd250c713cc9fff983e73a9c43253f
grzegorzsz24 <grzesio.szymanek@gmail.com>
2023-11-12 19:39:51 +0100

    backend: create message, channel entities and web socket config

---
+++ backend/AutomotiveApp/src/main/java/com/example/automotiveapp/domain/Channel.java
@@ -0,0 +1,20 @@
+package com.example.automotiveapp.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Channel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+}
