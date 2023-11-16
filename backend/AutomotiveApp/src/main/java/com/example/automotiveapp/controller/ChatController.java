3bc81378dfd7e22563ffec0218083743cfaf1326
grzegorzsz24 <grzesio.szymanek@gmail.com>
2023-11-16 21:44:21 +0100

    backend: remove test data for messages and friends

--- backend/AutomotiveApp/src/main/java/com/example/automotiveapp/controller/ChatController.java
+++ backend/AutomotiveApp/src/main/java/com/example/automotiveapp/controller/ChatController.java
@@ -7,6 +7,7 @@ import com.example.automotiveapp.mapper.MessageDtoMapper;
 import com.example.automotiveapp.service.ChannelService;
 import com.example.automotiveapp.service.MessageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
